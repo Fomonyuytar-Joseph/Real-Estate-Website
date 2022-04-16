b79db2e7cbfc2706428e4f6a4bbe308c03c457f4
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-04-16 05:49:42 +0100

    init

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -95,7 +95,7 @@ function Cards() {
       <div class='col-md-3'>
 
       <div class="card" style={{width: '20rem'}}>
-  <img src={im1} class="card-img-top" alt="..."/>
+  <img src={im2} class="card-img-top" alt="..."/>
   <div class="card-img-overlay d-flex flex-row justify-content-between z-index-1">
     <div>
     <button type='button' class="btn card-title " style={ButtonStyle}>FEATURING</button>
@@ -108,22 +108,22 @@ function Cards() {
     
   </div>
   <div class='d-flex flex-row justify-content-end z-index-2 ' style={{marginBottom:'1px'}}>
-  <img src={p1}  alt="........." class='rounded-circle img-thumbnail' style={ImageStyle} />
+  <img src={p2}  alt="........." class='rounded-circle img-thumbnail' style={ImageStyle} />
   </div>
   <div class="card-body">
-    <p class="card-text" style={TextStyle}>Lily Apartment</p>
+    <p class="card-text" style={TextStyle}>Your Lux House</p>
     <p class="card-text" >120m² Chavachavdae , Ave 21</p>
 
 
     <div class='d-flex flex-row justify-content-between align-items-center ' >
       <div >
       <span  style={iconStyle }><FontAwesomeIcon icon={faBed} /></span>
-      <p style={CardTextStyle}>3 beds</p>
+      <p style={CardTextStyle}>4 beds</p>
       </div>
 
       <div>
       <span style={iconStyle}><FontAwesomeIcon icon={faBath} /></span>
-      <p style={CardTextStyle}>2 Baths</p>
+      <p style={CardTextStyle}>4 Baths</p>
       </div>
 
       <div>
@@ -133,7 +133,7 @@ function Cards() {
 
       <div>
       <span style={iconStyle}><FontAwesomeIcon icon={faSignal} /></span>
-      <p style={CardTextStyle} >12th floor</p>
+      <p style={CardTextStyle} >19th floor</p>
       </div>
 
 
@@ -147,7 +147,7 @@ function Cards() {
   </div>
 
   <div class="card-footer d-flex  flex-row justify-content-between bg-transparent">
-  <div><h5 style={style}>$100.000</h5></div>  
+  <div><h5 style={style}>$180.000</h5></div>  
 
     <div>
       <span style={iconStyle}><FontAwesomeIcon icon={faHeart} /></span>
@@ -165,7 +165,7 @@ function Cards() {
       <div class='col-md-3'>
 
       <div class="card" style={{width: '20rem'}}>
-  <img src={im1} class="card-img-top" alt="..."/>
+  <img src={im3} class="card-img-top" alt="..."/>
   <div class="card-img-overlay d-flex flex-row justify-content-between z-index-1">
     <div>
     <button type='button' class="btn card-title " style={ButtonStyle}>FEATURING</button>
@@ -178,22 +178,22 @@ function Cards() {
     
   </div>
   <div class='d-flex flex-row justify-content-end z-index-2 ' style={{marginBottom:'1px'}}>
-  <img src={p1}  alt="........." class='rounded-circle img-thumbnail' style={ImageStyle} />
+  <img src={p3}  alt="........." class='rounded-circle img-thumbnail' style={ImageStyle} />
   </div>
   <div class="card-body">
-    <p class="card-text" style={TextStyle}>Lily Apartment</p>
-    <p class="card-text" >120m² Chavachavdae , Ave 21</p>
+    <p class="card-text" style={TextStyle}>Apartment At The Heart Of The City </p>
+    <p class="card-text" >90m² Rustaveli , Ave 2</p>
 
 
     <div class='d-flex flex-row justify-content-between align-items-center ' >
       <div >
       <span  style={iconStyle }><FontAwesomeIcon icon={faBed} /></span>
-      <p style={CardTextStyle}>3 beds</p>
+      <p style={CardTextStyle}>1 beds</p>
       </div>
 
       <div>
       <span style={iconStyle}><FontAwesomeIcon icon={faBath} /></span>
-      <p style={CardTextStyle}>2 Baths</p>
+      <p style={CardTextStyle}>1 Baths</p>
       </div>
 
       <div>
@@ -203,7 +203,7 @@ function Cards() {
 
       <div>
       <span style={iconStyle}><FontAwesomeIcon icon={faSignal} /></span>
-      <p style={CardTextStyle} >12th floor</p>
+      <p style={CardTextStyle} >2th floor</p>
       </div>
 
 
